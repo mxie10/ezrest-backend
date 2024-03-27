559fa8d9bf5ddabe74638bfe79bd58639c478d38
mxie10 <xieyz13@gmail.com>
2024-03-27 10:06:56 -0400

    fix issues

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,6 @@ const ListingController = require('../controllers/listingController');
 const passport = require('passport');
 
 // router.get('/', passport.authenticate("jwt", { session: false }), ListingController.getListings);
-router.get('/', ListingController.getAllListings);
+router.get('/',  passport.authenticate("jwt", { session: false }),ListingController.getAllListings);
 router.post('/', passport.authenticate("jwt", { session: false }), ListingController.postListings);
 module.exports = router;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
+/**
+ * Test suite for the getAllListings method in the ListingController.
+ * It ensures that the method returns all listings successfully.
+ * @author Mingyuan Xie
+ */
+
 const Listing = require('../models/listing');
 const User = require('../models/user');
 const ListingController = require('../controllers/listingController');
@@ -22,23 +28,28 @@ describe('getAllListings', () => {
     });
 });
 
+/**
+ * Test suite for the getUsers method in the AuthController.
+ * It ensures that the method returns all user successfully.
+ * @author Mingyuan Xie
+ */
 
-// describe('getUser', () => {
-//     it('should return specific users', async () => {
-//         const mockQuery = {
-//             exec: jest.fn().mockResolvedValue([{ user: '65f8562c2b04018ecfd595b8' }])
-//         };
-//         User.find = jest.fn().mockReturnValue(mockQuery);
+describe('getUser', () => {
+    it('should return specific users', async () => {
+        const mockQuery = {
+            exec: jest.fn().mockResolvedValue([{ user: '65f8562c2b04018ecfd595b8' }])
+        };
+        User.find = jest.fn().mockReturnValue(mockQuery);
 
-//         const mockRes = {
-//             status: jest.fn().mockReturnThis(),
-//             json: jest.fn()
-//         };
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
 
-//         await AuthController.getUser(null, mockRes);
+        await AuthController.getUser(null, mockRes);
 
-//         // Assertions
-//         expect(mockRes.status).toHaveBeenCalledWith(200);
-//         expect(mockRes.json).toHaveBeenCalledWith({ success: true, data: [{ _id: '65f8562c2b04018ecfd595b8' }] });
-//     });
-// });
+        // Assertions
+        expect(mockRes.status).toHaveBeenCalledWith(200);
+        expect(mockRes.json).toHaveBeenCalledWith({ success: true, data: [{ _id: '65f8562c2b04018ecfd595b8' }] });
+    });
+});
